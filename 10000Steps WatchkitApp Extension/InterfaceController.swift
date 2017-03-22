65213101ce562dbcd6f1d02a4c7668ee3acfe1e8
eCorrea831 <erica.correa831@gmail.com>
2017-03-21 20:03:20 -0400

    Create struct to hold necessary gif data and add the gifs to celebrating and waiting arrays:

--- 10000Steps WatchkitApp Extension/InterfaceController.swift
+++ 10000Steps WatchkitApp Extension/InterfaceController.swift
@@ -8,15 +8,42 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet var numberOfStepsLabel: WKInterfaceLabel!
     @IBOutlet var andCountingLabel: WKInterfaceLabel!
 
-    var steps = 9000
+    var steps = 10000
     var hasReached10000 = false
-    let waitingGifs = [UIImage]()
-    let celebratingGifs = [UIImage]()
+
+    let waitingGifs = [
+        gif.init(name: "waiting1frame", length: 10, duration: 1),
+        gif.init(name: "waiting2frame", length: 7, duration: 1),
+        gif.init(name: "waiting3frame", length: 23, duration: 3),
+        gif.init(name: "waiting4frame", length: 180, duration: 5),
+        gif.init(name: "waiting5frame", length: 16, duration: 1),
+        gif.init(name: "waiting6frame", length: 10, duration: 1.5),
+        gif.init(name: "waiting7frame", length: 114, duration: 3),
+        gif.init(name: "waiting8frame", length: 59, duration: 3),
+        gif.init(name: "waiting9frame", length: 58, duration: 4),
+        gif.init(name: "waiting10frame", length: 23, duration: 2)
+    ]
+
+    let celebratingGifs = [
+        gif.init(name: "celebrating1frame", length: 6, duration: 1),
+        gif.init(name: "celebrating2frame", length: 22, duration: 1.5),
+        gif.init(name: "celebrating3frame", length: 14, duration: 1),
+        gif.init(name: "celebrating4frame", length: 29, duration: 1.5),
+        gif.init(name: "celebrating5frame", length: 6, duration: 1),
+        gif.init(name: "celebrating6frame", length: 40, duration: 1.5),
+        gif.init(name: "celebrating7frame", length: 15, duration: 3),
+        gif.init(name: "celebrating8frame", length: 22, duration: 1),
+        gif.init(name: "celebrating9frame", length: 23, duration: 2),
+        gif.init(name: "celebrating10frame", length: 41, duration: 4)
+    ]
+
     
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
 
-        steps > 9999 ? setUpCelebratingView() : setUpWaitingView()
+        if steps > 9999 { hasReached10000 = true }
+        else { hasReached10000 = false }
+        hasReached10000 ? setUpCelebratingView() : setUpWaitingView()
     }
 
     func setUpWaitingView() {
@@ -25,6 +52,10 @@ class InterfaceController: WKInterfaceController {
 
         let formattedSteps = formatNumber(of: steps)
         headerLabel.setText("\(formattedSteps) Steps")
+
+        let gif = waitingGifs[0]
+        gifImage.setImageNamed(gif.name)
+        gifImage.startAnimatingWithImages(in: NSRange(location: 0, length: gif.length), duration: gif.duration, repeatCount: Int.max)
     }
 
     func setUpCelebratingView() {
@@ -34,6 +65,10 @@ class InterfaceController: WKInterfaceController {
         headerLabel.setText("YOU DID IT!")
         let formattedSteps = formatNumber(of: steps)
         numberOfStepsLabel.setText("\(formattedSteps) Steps")
+
+        let gif = celebratingGifs[0]
+        gifImage.setImageNamed(gif.name)
+        gifImage.startAnimatingWithImages(in: NSRange(location: 0, length: gif.length), duration: gif.duration, repeatCount: Int.max)
     }
 
     func formatNumber(of steps: Int) -> String {
@@ -52,4 +87,10 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         super.didDeactivate()
     }
+}
+
+struct gif {
+    let name: String
+    let length: Int
+    let duration: Double
 }
